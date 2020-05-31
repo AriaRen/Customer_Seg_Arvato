9f1a83085ef292b981c15b8409e61cb79ec2a98d
Aria_Ren <r.sining@wustl.edu>
2020-05-31 13:48:02 -0700

    Create Arvato_Cus_Seg.py

---
+++ Arvato_Cus_Seg.py
@@ -0,0 +1,29 @@
+# import libraries here; add more as necessary
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# magic word for producing visualizations in notebook
+%matplotlib inline
+
+
+#Load Data
+azdias = pd.read_csv('../../data/Term2/capstone/arvato_data/Udacity_AZDIAS_052018.csv', sep=';')
+customers = pd.read_csv('../../data/Term2/capstone/arvato_data/Udacity_CUSTOMERS_052018.csv', sep=';')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
