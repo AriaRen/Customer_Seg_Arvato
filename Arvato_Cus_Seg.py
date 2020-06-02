b2c686ac974cf4edce7f7ff74ac7dff9a91e014f
Aria_Ren <r.sining@wustl.edu>
2020-06-01 21:58:38 -0700

    Update Arvato_Cus_Seg.py

--- Arvato_Cus_Seg.py
+++ Arvato_Cus_Seg.py
@@ -12,7 +12,10 @@ import seaborn as sns
 azdias = pd.read_csv('../../data/Term2/capstone/arvato_data/Udacity_AZDIAS_052018.csv', sep=';')
 customers = pd.read_csv('../../data/Term2/capstone/arvato_data/Udacity_CUSTOMERS_052018.csv', sep=';')
 
-
+#Clean data and remove NA.
+azdias = azdias.dropna(axis=1, thresh=.7*azdias.shape[0])
+azdias = azdias.dropna(axis=0, thresh=.7*azdias.shape[1])
+azdias = azdias.dropna()
 
 
 
